c0d427204aab55b7f5f66b2d14b6e0b54bf98f1f
manjunath <manjunath@git>
2024-03-15 13:12:53 +0530

    remove webpack

--- src/app.js
+++ src/app.js
@@ -6,19 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
-const webpackConfig = require('../webpack.config');
 
 var app = express();
-// const compiler = webpack(webpackConfig);
-
-// app.use(webpackDevMiddleware(compiler, {
-//   publicPath: webpackConfig.output.publicPath
-// }));
-
-// app.use(webpackHotMiddleware(compiler));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
